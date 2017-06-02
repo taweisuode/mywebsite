0cf107c4b22efb1f35f3043d3c255adaef174b06
Pengge Zhuang <1064480036@qq.com>
2017-06-02 17:40:40 +0800

    feature:添加ppf框架指导说明

---
+++ Application/Guide/Controller/GeneralController.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @desc ppf框架用户手册
+ */
+    class GeneralController extends Controller {
+        public function welcomeAction() {
+            $this->view->show();
+        }
+    }
+?>
